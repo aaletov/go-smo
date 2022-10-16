3b887657d50f3b6050743befd5987fdd22a5192f
Artemy Pozdnyakov <52921276+aaletov@users.noreply.github.com>
2022-10-17 02:07:29 +0300

    Implement poisson distribution (#7)
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>

--- pkg/source/source.go
+++ pkg/source/source.go
@@ -2,23 +2,49 @@ package source
 
 import (
 	"time"
+
+	"github.com/aaletov/go-smo/pkg/clock"
 	"github.com/aaletov/go-smo/pkg/request"
+	"golang.org/x/exp/rand"
+	"gonum.org/v1/gonum/stat/distuv"
 )
 
 type Source interface {
 	GetRequest() (*request.Request, time.Time)
 }
 
+var (
+	sourcesCount int32 = 0
+	randSource         = rand.NewSource(uint64(time.Now().UnixNano()))
+)
+
+// Lambda is an expected value of time passed until next req
 func NewSource(lambda time.Duration) Source {
-	return sourceImpl{
-		lambda: lambda,
+	sourceNumber := sourcesCount + 1
+	sourcesCount++
+	return &sourceImpl{
+		sourceNumber:  sourceNumber,
+		lastReqNumber: 0,
+		lastGenTime:   clock.SMOClock.Time,
+		lambda:        lambda,
+		gen: distuv.Poisson{
+			Lambda: float64(lambda),
+			Src:    randSource,
+		},
 	}
 }
 
 type sourceImpl struct {
-	lambda time.Duration
+	sourceNumber  int32
+	lastReqNumber int32
+	lastGenTime   time.Time
+	lambda        time.Duration
+	gen           distuv.Poisson
 }
 
-func (s sourceImpl) GetRequest() (*request.Request, time.Time) {
-	return nil, time.Time{}
+func (s *sourceImpl) GetRequest() (*request.Request, time.Time) {
+	duration := time.Duration(int64(s.gen.Rand()))
+	time := s.lastGenTime.Add(duration)
+	s.lastGenTime = time
+	return &request.Request{}, time
 }
---
+++ pkg/source/source_test.go
@@ -0,0 +1,22 @@
+package source
+
+import (
+	"testing"
+	"time"
+
+	"github.com/aaletov/go-smo/pkg/clock"
+)
+
+func getSource() Source {
+	clock.InitClock(time.Now())
+	return NewSource(time.Duration(10))
+}
+
+func TestNewSource(t *testing.T) {
+	_ = getSource()
+}
+
+func TestGetRequest(t *testing.T) {
+	source := getSource()
+	_, _ = source.GetRequest()
+}
