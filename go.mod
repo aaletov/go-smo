90fa71ffdc1a833795d740a0ff2d9d7695186896
Artemy Pozdnyakov <52921276+aaletov@users.noreply.github.com>
2022-10-17 02:08:22 +0300

    Revert "Implement poisson distribution (#7)"
    This reverts commit 3b887657d50f3b6050743befd5987fdd22a5192f.

--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module github.com/aaletov/go-smo
 
 go 1.18
 
-require gonum.org/v1/gonum v0.12.0
-
 require golang.org/x/exp v0.0.0-20221012211006-4de253d81b95 // indirect
--- pkg/source/source.go
+++ pkg/source/source.go
@@ -2,49 +2,23 @@ package source
 
 import (
 	"time"
-
-	"github.com/aaletov/go-smo/pkg/clock"
 	"github.com/aaletov/go-smo/pkg/request"
-	"golang.org/x/exp/rand"
-	"gonum.org/v1/gonum/stat/distuv"
 )
 
 type Source interface {
 	GetRequest() (*request.Request, time.Time)
 }
 
-var (
-	sourcesCount int32 = 0
-	randSource         = rand.NewSource(uint64(time.Now().UnixNano()))
-)
-
-// Lambda is an expected value of time passed until next req
 func NewSource(lambda time.Duration) Source {
-	sourceNumber := sourcesCount + 1
-	sourcesCount++
-	return &sourceImpl{
-		sourceNumber:  sourceNumber,
-		lastReqNumber: 0,
-		lastGenTime:   clock.SMOClock.Time,
-		lambda:        lambda,
-		gen: distuv.Poisson{
-			Lambda: float64(lambda),
-			Src:    randSource,
-		},
+	return sourceImpl{
+		lambda: lambda,
 	}
 }
 
 type sourceImpl struct {
-	sourceNumber  int32
-	lastReqNumber int32
-	lastGenTime   time.Time
-	lambda        time.Duration
-	gen           distuv.Poisson
+	lambda time.Duration
 }
 
-func (s *sourceImpl) GetRequest() (*request.Request, time.Time) {
-	duration := time.Duration(int64(s.gen.Rand()))
-	time := s.lastGenTime.Add(duration)
-	s.lastGenTime = time
-	return &request.Request{}, time
+func (s sourceImpl) GetRequest() (*request.Request, time.Time) {
+	return nil, time.Time{}
 }
