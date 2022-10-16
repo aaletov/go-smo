26845f0cf7ca6ab59d8d61cd81827234d92055d7
Artemy Pozdnyakov <52921276+aaletov@users.noreply.github.com>
2022-10-16 22:18:27 +0300

    [POC-2] Implement priority queue (#4)
    * Implement methods
    
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>
    
    * Fix methods
    
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>
    
    * Test queue
    
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>
    
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>

--- pkg/queue/priority_queue.go
+++ pkg/queue/priority_queue.go
@@ -2,6 +2,7 @@ package queue
 
 import (
 	"container/list"
+	"errors"
 )
 
 type Comparable interface {
@@ -10,13 +11,36 @@ type Comparable interface {
 
 type PriorityQueue[T Comparable] interface {
 	Push(value T) error
-	Get(index int32) *T
-	Add(value T) 
+	Front() *QueueElement[T]
+	Back() *QueueElement[T]
+	Add(value T)
 	Pop() T
 }
 
+type QueueElement[T Comparable] struct {
+	ListElement *list.Element
+}
+
+func (e QueueElement[T]) Next() *QueueElement[T] {
+	if e.ListElement.Next() == nil {
+		return nil
+	}
+	return &QueueElement[T]{e.ListElement.Next()}
+}
+
+func (e QueueElement[T]) Prev() *QueueElement[T] {
+	if e.ListElement.Prev() == nil {
+		return nil
+	}
+	return &QueueElement[T]{e.ListElement.Prev()}
+}
+
+func (e QueueElement[T]) Get() T {
+	return e.ListElement.Value.(T)
+}
+
 type orderedList[T Comparable] struct {
-	*list.List
+	list *list.List
 }
 
 func NewPriorityQueue[T Comparable]() PriorityQueue[T] {
@@ -25,16 +49,40 @@ func NewPriorityQueue[T Comparable]() PriorityQueue[T] {
 }
 
 func (l *orderedList[T]) Push(value T) error {
-	return nil
+	back := l.list.Back()
+	if (back == nil) || back.Value.(T).Less(value) {
+		l.list.PushBack(value)
+		return nil
+	}
+
+	return errors.New("Value is smaller than the last element of queue")
 }
 
-func (l orderedList[T]) Get(index int32) *T {
-	return new(T)
+func (l orderedList[T]) Front() *QueueElement[T] {
+	if l.list.Front() == nil {
+		return nil
+	}
+	return &QueueElement[T]{l.list.Front()}
+}
+
+func (l orderedList[T]) Back() *QueueElement[T] {
+	if l.list.Back() == nil {
+		return nil
+	}
+	return &QueueElement[T]{l.list.Back()}
 }
 
 func (l *orderedList[T]) Add(value T) {
+	for el := l.Front(); el != nil; el = el.Next() {
+		if value.Less(el.Get()) {
+			l.list.InsertBefore(value, el.ListElement)
+			return
+		}
+	}
+	l.list.PushBack(value)
 }
 
 func (l orderedList[T]) Pop() T {
-	return *new(T)
+	front := l.list.Front()
+	return l.list.Remove(front).(T)
 }
