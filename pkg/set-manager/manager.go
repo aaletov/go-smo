c90d159eb319bfb793a5025554a4998251f0608f
aapozd <aapozdnyakove@gmail.com>
2022-10-23 00:57:15 +0300

    Add methods and fix reject
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>

--- pkg/set-manager/manager.go
+++ pkg/set-manager/manager.go
@@ -8,14 +8,13 @@ import (
 )
 
 type ReqWGT = request.ReqWGT
-type ReqWRT = request.ReqWRT
+type ReqSE = request.ReqSE
 type Queue = queue.PriorityQueue[*ReqWGT]
 type QueueEl = queue.QueueElement[*ReqWGT]
 
 type SetManager interface {
-	Collect()
-	ToBuffer()
-	GetRejectList() []ReqWRT
+	Iterate()
+	GetRejectList() []ReqSE
 }
 
 func NewSetManager(sources []source.Source, buffers []buffer.Buffer) SetManager {
@@ -24,7 +23,7 @@ func NewSetManager(sources []source.Source, buffers []buffer.Buffer) SetManager 
 		buffers:    buffers,
 		bufPtr:     0,
 		reqQueue:   queue.NewPriorityQueue[*ReqWGT](),
-		rejectList: make([]ReqWGT, 0),
+		rejectList: make([]ReqSE, 0),
 	}
 }
 
@@ -33,15 +32,18 @@ type setManagerImpl struct {
 	buffers    []buffer.Buffer
 	bufPtr     int
 	reqQueue   Queue
-	rejectList []ReqWRT
+	rejectList []ReqSE
 }
 
-func (s *setManagerImpl) Collect() {
+func (s *setManagerImpl) collectReqs() {
 	for _, src := range s.sources {
 		s.reqQueue.Add(src.Generate())
 	}
 }
 
+// isNewest() determines are all other sources have generated their reqs after
+// passed req. Returnes false if not all of other sources have generated next
+// request
 func isNewest(queue Queue, el *QueueEl, sourcesCount int) bool {
 	matchedSrc := make([]bool, sourcesCount)
 	matchedSrcCount := 0
@@ -58,26 +60,41 @@ func isNewest(queue Queue, el *QueueEl, sourcesCount int) bool {
 	return false
 }
 
-func (s *setManagerImpl) reject(req *ReqWGT) {
-	prevBufPtr := s.bufPtr
-	for {
-		s.bufPtr = (s.bufPtr + 1) % len(s.sources)
-		inBufReq := s.buffers[s.bufPtr].Get()
-		if inBufReq.Req.SourceNumber < req.Req.SourceNumber {
-			s.buffers[s.bufPtr].Pop(req.Time)
-			inBufReq.Time = req.Time
-			s.rejectList = append(s.rejectList, *inBufReq)
-			s.buffers[s.bufPtr].Add(req)
-		}
-		if s.bufPtr == prevBufPtr {
-			s.rejectList = append(s.rejectList, *req)
-			break
-		}
+func (s *setManagerImpl) movePtr() {
+	s.bufPtr = (s.bufPtr + 1) % len(s.buffers)
+}
 
+func (s setManagerImpl) currentBuf() buffer.Buffer {
+	return s.buffers[s.bufPtr]
+}
+
+func (s *setManagerImpl) reject(rwgt *ReqWGT) {
+	prevBufPtr := s.bufPtr
+	s.movePtr()
+	for ; s.bufPtr != prevBufPtr; s.movePtr() {
+		bufRwgt := s.currentBuf().Get()
+		if rwgt.Req.SourceNumber < bufRwgt.Req.SourceNumber {
+			s.currentBuf().Pop(rwgt.Time)
+			s.rejectList = append(s.rejectList, ReqSE{
+				Req:   bufRwgt.Req,
+				Start: bufRwgt.Time,
+				End:   rwgt.Time,
+			})
+			s.currentBuf().Add(rwgt)
+		}
+	}
+	if s.bufPtr == prevBufPtr {
+		s.rejectList = append(s.rejectList, ReqSE{
+			Req:   rwgt.Req,
+			Start: rwgt.Time,
+			End:   rwgt.Time,
+		})
 	}
 }
 
-func (s *setManagerImpl) ToBuffer() {
+func (s *setManagerImpl) Iterate() {
+	s.collectReqs()
+
 	for el := s.reqQueue.Front(); el != nil; el = el.Next() {
 		if !isNewest(s.reqQueue, el, len(s.sources)) {
 			break
@@ -93,6 +110,7 @@ func (s *setManagerImpl) ToBuffer() {
 		}
 		reqwgt := el.Get()
 		s.buffers[s.bufPtr].Add(reqwgt)
+		s.buffers[s.bufPtr].Pop(reqwgt.Time)
 		s.bufPtr = (s.bufPtr + 1) % len(s.sources)
 	}
 }
